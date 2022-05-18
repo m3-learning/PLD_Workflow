c2c21ec30d1c11da95778f8e87e55ce7c074728c
Yichen Guo <yig319@lehigh.edu>
2022-05-18 16:00:28 -0700

    update ParameterStatistics.py

--- ParameterStatistics.py
+++ ParameterStatistics.py
@@ -12,9 +12,12 @@ def PlotParameter(df_api, ls_resp, parameter):
 
     :param parameter: PLD experiment parameter to track and plot 
     :type parameter: str
+    
+    :param plot_type: specify which type of plot to show, default: 'normal', option: 'hover'
+    :type plot_type: str
     '''
     
-    time_stamp_list, metric_list = [], []
+    ID_list, time_stamp_list, metric_list = [], [], []
 
     for i in range(len(ls_resp[0].item)):
 
@@ -29,23 +32,29 @@ def PlotParameter(df_api, ls_resp, parameter):
             metric = 0
 
         if metric != 0:
-            metric = float(re.split('/', metric)[-1])
+            ID_list.append(labels['header']['Growth ID'])
+#             print(labels['header']['Growth ID'])
+
             date = labels['header']['Date']    
             time_stamp = date[-4:]+'-'+date[:2]+'-'+date[3:5]
-
             if 'time' in labels['header'].keys():
                 time = labels['header']['time']
                 time_stamp = date + ' ' + time
-
             time_stamp_list.append(time_stamp)
+#             print(time_stamp)
+
+            metric = float(re.split('/', metric)[-1])
             metric_list.append(metric)
-            
+
     df = pd.DataFrame({
+                   'ID': ID_list,
                    'Date': time_stamp_list,
                     parameter: metric_list
                   })
-
-    df = df.set_index(pd.DatetimeIndex(df.iloc[:,0]))
-    df.drop('Date', 1, inplace=True)
-    display(df.hvplot.scatter())
+    
+    df_hover = df.set_index(pd.DatetimeIndex(df.iloc[:,1]))
+    df_hover.drop('Date', 1, inplace=True)
+    ax = df_hover.hvplot.scatter(hover_cols=['ID'])
+    display(ax)
+        
     return df
